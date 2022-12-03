880f791f6a31e8e488dd1d9b5c2813b5b9122783
ChiemekaAnunkor <anunkorjr@gmail.com>
2022-12-02 21:41:38 -0600

    first commit

---
+++ client/main.js
@@ -0,0 +1,54 @@
+const complimentBtn = document.getElementById("complimentButton");
+const fortuneButton = document.getElementById("fortuneButton");
+const fortuneSubmit = document.getElementById("fortuneSubmit");
+const deleteButton = document.getElementById("deleteButton");
+
+const updateSubmit = document.getElementById("updateSubmit");
+
+const getCompliment = () => {
+  axios.get("http://localhost:4000/api/compliment/").then((res) => {
+    const data = res.data;
+    alert(data);
+  });
+};
+
+const getFortune = () => {
+  axios.get("http://localhost:4000/api/fortune/").then((res) => {
+    const data = res.data;
+    alert(data);
+  });
+};
+
+const postFortune = () => {
+  let inputs = document.getElementById("fortune").value;
+
+  axios.post("http://localhost:4000/api/fortune/", inputs).then((res) => {
+    const data = res.data;
+  });
+  console.log(inputs);
+};
+
+const updateFortune = () => {
+  let inputs = document.getElementById("fortuneupdate").value;
+  let index = document.getElementById("index").value;
+  let update = { fortune: inputs, index: index };
+  console.log(update);
+
+  axios.put("http://localhost:4000/api/fortune/", update).then((res) => {
+    const data = res.data;
+    alert("updated: " + data);
+  });
+  console.log(inputs);
+};
+const deleteFortune = () => {
+  axios.delete("http://localhost:4000/api/fortune/").then((res) => {
+    const data = res.data;
+    alert("we deleted:" + data);
+  });
+};
+
+complimentBtn.addEventListener("click", getCompliment);
+fortuneButton.addEventListener("click", getFortune);
+fortuneSubmit.addEventListener("click", postFortune);
+deleteButton.addEventListener("click", deleteFortune);
+updateSubmit.addEventListener("click", updateFortune);
---
+++ server/controller.js
@@ -0,0 +1,50 @@
+const fortune = [
+  "A beautiful, smart, and loving person will be coming into your life",
+  "A dubious friend may be an enemy in camouflage",
+  "A faithful friend is a strong defense",
+  "A feather in the hand is better than a bird in the air",
+  "A fresh start will put you on your way        ",
+];
+module.exports = {
+  getCompliment: (req, res) => {
+    const compliments = [
+      "Gee, you're a smart cookie!",
+      "Cool shirt!",
+      "Your Javascript skills are stellar.",
+    ];
+
+    // choose random compliment
+    let randomIndex = Math.floor(Math.random() * compliments.length);
+    let randomCompliment = compliments[randomIndex];
+
+    res.status(200).send(randomCompliment);
+  },
+  getFortune: (req, res) => {
+    let randomIndex = Math.floor(Math.random() * fortune.length);
+    let randomFortune = fortune[randomIndex];
+
+    res.status(200).send(randomFortune);
+  },
+  addFortune: (req, res) => {
+    const addFortune = req.body.fortune;
+    fortune.push(addFortune);
+    res.status(200).send(addFortune);
+  },
+  updateFortune: (req, res) => {
+    console.log(fortune);
+    const addFortune = req.body.fortune;
+    const indexFortune = req.body.index;
+    fortune.splice(indexFortune, 1, addFortune);
+    console.log(fortune);
+    res.status(200).send(addFortune);
+  },
+  deleteFortune: (req, res) => {
+    let randomIndex = Math.floor(Math.random() * fortune.length);
+    let randomFortune = fortune[randomIndex];
+    // compair first console.log with the second console.log
+    console.log(fortune);
+    fortune.splice(randomIndex, 1);
+    console.log(fortune);
+    res.status(200).send(randomFortune);
+  },
+};
